ec69a4ae9ade05d870f40395e8cf9f2ac9fc1c2b
Inna Kozhokar <inna.kozhokar@shopify.com>
2022-03-22 09:57:28 +0100

    implement to_array function

--- 1_toArray/toArray.js
+++ 1_toArray/toArray.js
@@ -1,3 +1,15 @@
-function toArray(obj) {}
+function toArray(obj) {
+  // create an empty array to store values
+  const collection = []
+
+  // create for...in statement which iterates over all enumerable properties of an object 
+  for (const key in obj) {
+    // use push method to append new elements to the end of the array
+    collection.push(
+      [key, obj[key]] // transforms key-value pair {a: 1} into array => ["a", 1]
+    )
+  }
+  return collection
+}
 
 module.exports = { toArray };
