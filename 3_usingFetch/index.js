f8d23444a34e325decedc8256e3b813789c8cb3f
Inna Kozhokar <inna.kozhokar@shopify.com>
2022-03-22 09:59:26 +0100

    use fetch

--- 3_usingFetch/index.js
+++ 3_usingFetch/index.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // TODO: on button click, usw fetch to get the data from the url below and console.log it in the browser
   // https://jsonplaceholder.typicode.com/posts/1
+  document.addEventListener("click", () => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((result) => {
+        return result.json()
+      })
+      .then((data) => {
+        console.log(data)
+      })
+  })
 });
