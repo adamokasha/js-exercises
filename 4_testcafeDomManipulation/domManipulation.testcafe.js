46b06cc94a33c649ac8232476a57eb01e250cd53
Inna Kozhokar <inna.kozhokar@shopify.com>
2022-03-29 16:12:08 +0200

    fix formatting

--- 4_testcafeDomManipulation/domManipulation.testcafe.js
+++ 4_testcafeDomManipulation/domManipulation.testcafe.js
@@ -6,21 +6,18 @@ fixture`TestCafe - DOM Manipulation`.page`http://127.0.0.1:5500/4_testcafeDomMan
 test('Check button colour', async (t) => {
   const btn = await Selector('.button')
   const color = btn.getStyleProperty('background-color')
-  
+
   await t.expect(color).eql('rgb(209, 65, 65)')
   await t.click(btn)
-
   await t.expect(color).eql('rgb(0, 128, 0)')
-
 });
- 
+
 test('Check button text', async (t) => {
   const btn = await Selector('.button')
   const text = btn.textContent
-  
+
   await t.expect(text).eql('Click here')
   await t.click(btn)
-
-  .expect(text).eql('Clicked')
+  await t.expect(text).eql('Clicked')
 
 })
