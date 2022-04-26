740bc6adbd50977bcfab67031622387e00bb01e9
Inna Kozhokar <inna.kozhokar@shopify.com>
2022-04-27 01:20:30 +0200

    implement exercise 7

--- 7_modal/Modal.js
+++ 7_modal/Modal.js
@@ -8,13 +8,13 @@ class Modal {
   init() {
     this.triggerEl.addEventListener("click", () => {
       // TODO: open the modal by selecting the modal element and adding the class "is-active"
-
+      this.modalEl.classList.add("is-active")
     });
 
     this.modalEl.addEventListener("click", (e) => {
       if (e.target.classList.contains("modal")) {
         // TODO: close the modal by selecting the modal element and removing the class "is-active"
-
+        this.modalEl.classList.remove("is-active")
       }
     });
   }
--- 7_modal/index.js
+++ 7_modal/index.js
@@ -1,22 +1,34 @@
 import List from './List.js'
 import Modal from './Modal.js'
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
   // 1. Select the trigger element (button)
+  const triggerEl = document.getElementsByClassName("modal-button")[0]
 
   // 2. Create a list using List class and add items to it
+  const destinations = new List()
+  destinations.add("Los Angeles")
+  destinations.add("Madrid")
+  destinations.add("Reykjavik")
+  destinations.add("Oslo")
 
   // 3. Create a div to hold the list
+  const contentEl = document.createElement("div")
 
   // 4. Add the class "modal-content" to the div
+  contentEl.classList.add("modal-content")
 
   // 5. Add the list items to the div
+  contentEl.appendChild(destinations.items)
 
   // 6. Select the modal element
+  const modalEl = document.getElementsByClassName("modal")[0]
 
   // 7. Create a new instance of Modal class and pass the trigger element, modal element and content element as arguments
+  const modal = new Modal(triggerEl, modalEl, contentEl)
 
   // 8. Initialize the Modal class
+  modal.init()
 
 })
